b0f876109cb5ada3987753a6d68a03cdd1623435
esailly <esailly@free.fr>
2016-06-17 08:50:01 +0100

    hahaha

--- src/Client.hpp
+++ src/Client.hpp
@@ -1,3 +1,13 @@
+/**
+ * \file Client.hpp
+ * \brief Classe Cliente de gestion des echanges réseau de BattleShip 
+ * \author Sailly E
+ * \version 0.9
+ * \date 16 juin 2016
+ *
+ * Classe de gestion des echanges reseau cote Client
+ *
+ */
 #ifndef CLIENT_HPP_
 #define CLIENT_HPP_
 
@@ -6,23 +16,39 @@
 #include "PacketType.hpp"
 #include "Joueur.hpp"
 
-
+/*! \class CLient
+   * \brief classe client reseau
+   *
+   *  La classe gere les connexion avec le serveur
+   */
 class Client
 {
 private:
-  sf::TcpSocket _mySocket;
-  bool _bloquant;
-  bool _joueurIsActif;
-  bool _closeRunWait;
+  sf::TcpSocket _mySocket; /*!<Socket de connection au serveur>*/ 
+  bool _bloquant;          /*!<Flag pour rendre le socket bloquant>*/
+  bool _joueurIsActif;     /*!<Flag pour signifier au programme principal de désactiver la zone de saisie>*/
+  bool _closeRunWait;      /*!<Flag pour demander au programme principal de fermer la fenetre>*/ 
   
          
 public:
 int posx, posy; //temporaire
 bool res;      //temporaire
-  
+
+  /*!
+     *  \brief Constructeur
+     *  Constructeur de la classe Client
+     *  \param
+     */
 	Client();
+  /*!
+     *  \brief Destructeur
+     *  Destructeur de la classe Client
+     */
 	~Client();
+
+  
   void setBloquant(bool bloquant);
+  
   bool getJoueurIsActif() const;
   void setJoueurIsActif(bool joueurIsActif);
   bool getCloseRunWait() const;
